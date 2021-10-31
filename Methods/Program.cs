ecaf9ee9ce8ed3caa1972fcd512044ef2d0756f8
AbdullahOzer <ozerabdullah11@gmail.com>
2021-10-31 14:38:39 +0300

    31.10.21

---
+++ Methods/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+/*Problemleri çözerken sık kullanılan methodlar*/
+namespace Methods
+{
+    public class Prime
+    {
+        /// <summary>
+        /// Returns the given number isPrime(true) or Not(False)
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        public static bool IsPrime(long number)
+        {
+            if (number < 4)
+            {
+                return true;
+            }
+            else if (Math.Sqrt(number) == Math.Ceiling(Math.Sqrt(number)))
+            {
+                return false;
+            }
+            for (long i = 2; i < Math.Ceiling(Math.Sqrt(number)); i++)
+            {
+                if (number % i == 0)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        /// <summary>
+        /// Returns a List of Divisors of Given number. Use it in foreachLoop
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns>System.Collections.Generic.List`1[System.Int64]</returns>
+        public static List<long> PrimeDivisors(long number)
+        {
+            List<long> PrimeDivisors_ = new List<long>(1);
+            for (long i = 2; i < Math.Ceiling(Math.Sqrt(number)); i++)
+            {
+                if (number%i==0 && IsPrime(i))
+                {
+                    PrimeDivisors_.Add(i);
+                }
+
+            }
+            return PrimeDivisors_;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+
+    }
+}
---
+++ Problem3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Methods;
+/* 600851475143 sayısının en büyük asal bölenini bulunuz*/
+namespace Problem3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Last index of List is Biggest Prime Divisor
+            Console.WriteLine(Prime.PrimeDivisors(600851475143)[Prime.PrimeDivisors(13195).Count - 1]);
+        }
+    }
+}
