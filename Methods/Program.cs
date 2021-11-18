db0a229da563c105eb3de99a30e72f9fc03b2e58
AbdullahOzer <ozerabdullah11@gmail.com>
2021-11-19 00:20:06 +0300

    19_11_2021

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -39,13 +39,35 @@ namespace Methods
             List<long> PrimeDivisors_ = new List<long>(1);
             for (long i = 2; i < Math.Ceiling(Math.Sqrt(number)); i++)
             {
-                if (number%i==0 && IsPrime(i))
+                if (number % i == 0 && IsPrime(i))
                 {
                     PrimeDivisors_.Add(i);
                 }
 
             }
             return PrimeDivisors_;
+        }
+        public static List<long> PrimeDivisorsAll(long number)
+        {
+
+            List<long> PrimeDivisorsAll_ = new List<long>();
+            if (IsPrime(number))
+            {
+                PrimeDivisorsAll_.Add(number);
+                return PrimeDivisorsAll_;
+            }
+            int divisor = 2;
+            while (divisor <= number)
+            {
+                if (IsPrime(divisor) && number % divisor == 0)
+                {
+                    PrimeDivisorsAll_.Add(divisor);
+                    number = number / divisor;
+                    continue;
+                }
+                divisor += 1;
+            }
+            return PrimeDivisorsAll_;
         }
     }
     public class Palindrome
@@ -60,6 +82,51 @@ namespace Methods
                 }
             }
             return true;
+        }
+    }
+    public class Gcd
+    {
+        public static int GetGcd(int a, int b)
+        {
+
+            while (a != 0 && b != 0)
+            {
+                int b_ = Math.Max(a, b) % Math.Min(a, b);
+                int a_ = Math.Min(a, b);
+                a = a_;
+                b = b_;
+            }
+            return Math.Max(a, b);
+        }
+
+    }
+    public class Lcm
+    {
+        public static int GetLcm(int a, int b)
+        {
+            return a * (b / Gcd.GetGcd(a, b));
+        }
+        public static int GetLcms(List<int> numbers)
+        {
+            if (numbers.Count < 2)
+            {
+                return numbers[0];
+            }
+            else if (numbers.Count == 2)
+            {
+                return GetLcm(numbers[0], numbers[1]);
+            }
+            else
+            {
+                int lcm = GetLcm(numbers[0], numbers[1]);
+                for (int i = 2; i < numbers.Count; i++)
+                {
+                    lcm = Lcm.GetLcm(lcm, numbers[i]);
+
+                }
+                return lcm;
+            }
+            
         }
     }
     class Program
---
+++ Problem5/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Methods;
+namespace Problem5
+{
+    //LCM(0,21)
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = new List<int>();
+            for (int i = 1; i < 21; i++)
+            {
+                numbers.Add(i);
+            }
+            
+            Console.WriteLine(Lcm.GetLcms(numbers));
+        }
+    }
+}
