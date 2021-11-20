0c5750183d0b620dd6ad152e0c8ff4cbe5a254d6
AbdullahOzer <ozerabdullah11@gmail.com>
2021-11-20 23:27:58 +0300

    20.11.21.4

--- Problem9/Program.cs
+++ Problem9/Program.cs
@@ -7,20 +7,23 @@ namespace Problem9
     {
         static void Main(string[] args)
         {
-            
-            for (int b = 2; b < 500; b++)
+
+            for (int b = 2; b < 500; b++)    //500+500+100==1100 nearly 1000 so take b<500
             {
-                for (int a = 1; a < b; a++)
+                for (int a = 1; a < b; a++)  //a<b<c
                 {
                     double c = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
-                    if ((Math.Pow(a, 2) + Math.Pow(b, 2) == Math.Pow(c,2)) && (a + b + c == 1000.0))
+                    if ((Math.Pow(a, 2) + Math.Pow(b, 2) == Math.Pow(c, 2)) && (a + b + c == 1000.0))
                     {
-                        Console.WriteLine(a*b*c);
+                        Console.WriteLine(a);
+                        Console.WriteLine(b);
+                        Console.WriteLine(c);
+                        Console.WriteLine(a * b * c);
                         break;
                     }
                 }
             }
-            
+
 
 
 
