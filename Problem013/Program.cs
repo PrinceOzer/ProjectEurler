32de59d0ec0692858d2e0ae6d64e2a731081c387
AbdullahOzer <ozerabdullah11@gmail.com>
2021-11-28 01:47:15 +0300

    27_11_2021_1

--- Problem013/Program.cs
+++ Problem013/Program.cs
@@ -6,7 +6,22 @@ namespace Problem013
     {
         static void Main(string[] args)
         {
-            
+            string[] str = System.IO.File.ReadAllLines(@"C:\Users\okkes\source\repos\ProjectEurler\Problem013\numbers\numbers.txt");
+            string sum = "";
+            int sum_ = 0;
+            for (int i = 49; i >= 0; i--)
+            {
+                int subSum = 0;
+                for (int j = 0; j < 100; j ++)
+                {
+                    subSum += int.Parse(str[j][i].ToString()); 
+                }
+                subSum += sum_;
+                sum = sum.Insert(0, (subSum % 10).ToString()) ;
+                sum_ = (subSum - (subSum % 10)) / 10;
+            }
+            sum = sum.Insert(0, sum_.ToString());
+            Console.WriteLine(sum.Substring(0,10));
         }
     }
 }
